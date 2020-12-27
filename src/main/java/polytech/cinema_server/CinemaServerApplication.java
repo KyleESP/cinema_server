d1fc724aaa686a430b3450aaa6ff19f44bbe7bc3
Kyle <espkyle@gmail.com>
2020-12-27 22:45:20 +0100

    Init spring boot server

---
+++ src/main/java/polytech/cinema_server/CinemaServerApplication.java
@@ -0,0 +1,13 @@
+package polytech.cinema_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/polytech/cinema_server/CinemaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package polytech.cinema_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
