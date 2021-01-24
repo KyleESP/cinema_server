59e951a2de62dbe0ec9793287dcd710e27d9853e
Tom BEFIEUX <tom.befieux@gmail.com>
2021-01-24 22:28:44 +0100

    disable csrf

--- src/main/java/polytech/cinema_server/security/SecurityConfig.java
+++ src/main/java/polytech/cinema_server/security/SecurityConfig.java
@@ -22,7 +22,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http.cors().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/**").permitAll();
     }
 
     @Bean
